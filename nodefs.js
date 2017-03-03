a74cf0af422c939719a302c743e8ed060977c38f
Jufebrown <jufebrown@gmail.com>
2017-03-03 09:54:29 -0600

    added sync fileread

--- nodefs.js
+++ nodefs.js
@@ -1,1 +1,19 @@
 #!/usr/bin/env node
+
+const {readFileSync} = require('fs')
+
+const fileArg = process.argv[2]
+
+if (fileArg) {
+  try {
+    const data = readFileSync(fileArg, "")
+    process.stdout.write(data)
+  } catch(e) {
+    // statements
+    console.log('error:', e.stack);
+  }
+} else {
+  return;
+}
+
+console.log('This is the synchronous version.')
