146087195750f51d7b86cc6c33da2497886b8144
Jufebrown <jufebrown@gmail.com>
2017-03-03 10:13:18 -0600

    added async file read

--- nodefs.js
+++ nodefs.js
@@ -1,19 +1,43 @@
 #!/usr/bin/env node
 
-const {readFileSync} = require('fs')
+/***************************
+Blocking example
+***************************/
+
+// const {readFileSync} = require('fs')
+
+// const fileArg = process.argv[2]
+
+// if (fileArg) {
+//   try {
+//     const data = readFileSync(fileArg, "UTF8")//defaults UTF8 encoding (so we don't need UTF8 here)
+//     process.stdout.write(data)
+//   } catch(e) {
+//     // statements
+//     console.log('error:', e.stack);
+//   }
+// } else {
+//   process.exit()
+// }
+
+// console.log('This is the synchronous version.')
+
+
+/****************************
+Non-blocking example
+****************************/
+
+const {readFile} = require('fs')
 
 const fileArg = process.argv[2]
 
 if (fileArg) {
-  try {
-    const data = readFileSync(fileArg, "")
+  readFile(fileArg, (err, data) => {
+    if (err) return console.error(err)
     process.stdout.write(data)
-  } catch(e) {
-    // statements
-    console.log('error:', e.stack);
-  }
+  })
 } else {
-  return;
+  process.exit()
 }
 
-console.log('This is the synchronous version.')
+console.log('This is the async version.')
